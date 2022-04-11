147ed0c5f556f53a6a5dd16b0eec4251128579e9
Dezay <dezay11@gmail.com>
2022-04-11 11:35:20 +0530

    Add files via upload

---
+++ Plot 1.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(lubridate)
+
+if(!dir.exists("data")) { dir.create("data") }
+
+
+data.url   <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+data.path  <- "data/household_power_consumption.zip"
+data.unzip <- "data/household_power_consumption.txt"
+
+if(!file.exists(data.path) & !file.exists(data.unzip)) {
+  download.file(data.url, data.path)
+  unzip(data.path, exdir = "data")
+}
+
+
+data <- read_delim("data/household_power_consumption.txt",
+                   delim = ";",
+                   na    = c("?"),
+                   col_types = list(col_date(format = "%d/%m/%Y"),
+                                    col_time(format = ""),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number())) %>%
+  filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
+
+
+hist(data$Global_active_power,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col  = "light blue")
+
+
+dev.copy(png, "Plot 1.png",
+         width  = 480,
+         height = 480)
+
+dev.off()
+
+rm(list = ls())
---
+++ Plot 3.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(lubridate)
+
+if(!dir.exists("data")) { dir.create("data") }
+
+
+data.url   <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+data.path  <- "data/household_power_consumption.zip"
+data.unzip <- "data/household_power_consumption.txt"
+
+if(!file.exists(data.path) & !file.exists(data.unzip)) {
+  download.file(data.url, data.path)
+  unzip(data.path, exdir = "data")
+}
+
+
+data <- read_delim("data/household_power_consumption.txt",
+                   delim = ";",
+                   na    = c("?"),
+                   col_types = list(col_date(format = "%d/%m/%Y"),
+                                    col_time(format = ""),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number(),
+                                    col_number())) %>%
+  filter(between(Date, as.Date("2007-02-01"), as.Date("2007-02-02")))
+
+
+data <- mutate(data, datetime = ymd_hms(paste(Date, Time)))
+
+
+png("Plot 4.png",
+    width  = 480,
+    height = 480)
+
+par(mfrow = c(2, 2))
+
+plot(Global_active_power ~ datetime, data, type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = NA)
+
+plot(Voltage ~ datetime, data, type = "l")
+
+plot(Sub_metering_1 ~ datetime, data, type = "l",
+     ylab = "Energy sub metering",
+     xlab = NA)
+
+lines(Sub_metering_2 ~ datetime, data, type = "l", col = "red")
+
+lines(Sub_metering_3 ~ datetime, data, type = "l", col = "blue")
+
+legend("topright",
+       col = c("black",
+               "red",
+               "blue"),
+       legend = c("Sub_metering_1",
+                  "Sub_metering_2",
+                  "Sub_metering_3"),
+       lty = 1,
+       bty = "n")
+
+plot(Global_reactive_power ~ datetime, data, type = "l")
+
+dev.off()
+
+rm(list = ls())
